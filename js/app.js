b3b937e616f3a4513a2d033ff80f97d57eb5146a
realRojSerbest <49933115+realRojSerbest@users.noreply.github.com>
2020-03-22 19:44:57 +0300

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,29 @@
+function world() {
+    $.getJSON("https://corona.lmao.ninja/all", function(data) {
+        var cases = data.cases;
+        var deaths = data.deaths;
+        var recovered = data.recovered;
+
+        $("button#world-cases").html($("button#world-cases").html().replace("{{data}}", cases));
+        $("button#world-deaths").html($("button#world-deaths").html().replace("{{data}}", deaths));
+        $("button#world-recovered").html($("button#world-recovered").html().replace("{{data}}", recovered));
+    });
+}
+
+function country(cn) {
+    $.getJSON("https://corona.lmao.ninja/countries/" + cn, function(data) {
+        var cases = data.cases;
+        var deaths = data.deaths;
+        var recovered = data.recovered;
+        
+        var todayCases = data.todayCases;
+        var todayDeaths = data.todayDeaths;
+
+        $("button#" + cn + "-cases").html($("button#" + cn + "-cases").html().replace("{{data}}", cases));
+        $("button#" + cn + "-deaths").html($("button#" + cn + "-deaths").html().replace("{{data}}", deaths));
+        $("button#" + cn + "-recovered").html($("button#" + cn + "-recovered").html().replace("{{data}}", recovered));
+
+        $("button#" + cn + "-todayCases").html($("button#" + cn + "-todayCases").html().replace("{{data}}", todayCases));
+        $("button#" + cn + "-todayDeaths").html($("button#" + cn + "-todayDeaths").html().replace("{{data}}", todayDeaths));
+    });
+}
